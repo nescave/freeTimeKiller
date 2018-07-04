5058446657ea5f449baa5e41d3a7f7df7d2dde33
Borysewicz <33198052+mriperm@gmail.com>
2018-07-04 23:03:48 +0200

    Poprawiony player movement

--- timeKiller/Assets/Resources/scripts/gameControlers/playerMovement.cs
+++ timeKiller/Assets/Resources/scripts/gameControlers/playerMovement.cs
@@ -3,8 +3,9 @@
 public class playerMovement : MonoBehaviour {
 
 	public float movementSpeed = 5.0f;
-	public CharacterController controller;
+	private CharacterController controller;
 	private Vector3 movementVector;
+	public float gravity = 200.0f;
 
 	private Transform cam;
 	public Vector3 camPos;
@@ -22,14 +23,17 @@ public class playerMovement : MonoBehaviour {
 
 	void Update () {
 
+		controller = GetComponent<CharacterController>();
 		cam.GetComponent<cameraMovement>().camfixedLocation = transform.position + camPos;
 
-		Debug.Log(controller.isGrounded);
-
 		movementVector = new  Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical")); 
+		if (Mathf.Abs(movementVector.x) + Mathf.Abs(movementVector.z) == 2){
+			movementVector *= 0.6f;  // fixing the bug where going both forward and sideways made player move faster
+		}
+
 		movementVector = transform.TransformDirection(movementVector);
 		movementVector *= movementSpeed;
-		
+		movementVector.y -= gravity * Time.deltaTime;
 		controller.Move(movementVector * Time.deltaTime);
 	}
 }
