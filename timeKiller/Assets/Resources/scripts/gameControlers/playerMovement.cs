e98641577dad0fec483df003cdc2e598b0b37c6a
Borysewicz <33198052+mriperm@gmail.com>
2018-07-04 21:18:44 +0200

    Added basic player movement - this time it's working.

--- timeKiller/Assets/Resources/scripts/gameControlers/playerMovement.cs
+++ timeKiller/Assets/Resources/scripts/gameControlers/playerMovement.cs
@@ -5,6 +5,7 @@ public class playerMovement : MonoBehaviour {
 	public float movementSpeed = 5.0f;
 	public CharacterController controller;
 	private Vector3 movementVector;
+	
 	void Start () {
 		
 	}
@@ -13,7 +14,7 @@ public class playerMovement : MonoBehaviour {
 	void Update () {
 		Debug.Log(controller.isGrounded);
 
-		movementVector = new  Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
+		movementVector = new  Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical")); 
 		movementVector = transform.TransformDirection(movementVector);
 		movementVector *= movementSpeed;
 		
