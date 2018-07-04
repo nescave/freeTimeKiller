4b12b996913d30c596e712c601664b15954a0716
kubaMikicki <jakub.mikicki@tensquaregames.com>
2018-07-04 22:38:39 +0200

    camera control merged with movement

--- timeKiller/Assets/Resources/scripts/gameControlers/playerMovement.cs
+++ timeKiller/Assets/Resources/scripts/gameControlers/playerMovement.cs
@@ -5,13 +5,25 @@ public class playerMovement : MonoBehaviour {
 	public float movementSpeed = 5.0f;
 	public CharacterController controller;
 	private Vector3 movementVector;
+
+	private Transform cam;
+	public Vector3 camPos;
 	
+	private void Awake() {
+
+	}
+
 	void Start () {
-		
+		cam = transform.GetChild(0);
+		camPos = cam.transform.localPosition /2;
+		//cam.transform.position = Vector3.zero;
 	}
 	
 
 	void Update () {
+
+		cam.GetComponent<cameraMovement>().camfixedLocation = transform.position + camPos;
+
 		Debug.Log(controller.isGrounded);
 
 		movementVector = new  Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical")); 
