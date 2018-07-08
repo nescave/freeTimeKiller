709541c5a049e4c531a6cde8a9b01eb6ea96f8c8
kubaMikicki <jakub.mikicki@tensquaregames.com>
2018-07-08 22:22:19 +0200

    enemy behaviour

---
+++ timeKiller/Assets/Resources/scripts/enemies/enemy.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[System.Serializable]
+public class enemy : enemyBehaviours {
+
+	public bool isWalking = false;
+	public bool reachingTarget = false;
+
+	private float rnd;
+	private float cooldown = 0;
+
+	public float distance;
+
+	private Vector3 randomPos;
+	private NavMeshAgent agent;
+	private SphereCollider detector;
+	private RaycastHit hit;
+
+	void Start () {
+		randomPos = transform.position;
+		agent = GetComponent<NavMeshAgent>();
+		path = new NavMeshPath();
+		detector = GetComponent<SphereCollider>();
+	}
+	
+	void Update () {
+
+		if(target != null && !reachingTarget){
+			Incoming();
+		}
+		if(target != null && reachingTarget){
+			distance = agent.remainingDistance;
+			agent.SetDestination(target.position);
+			if(distance >  8f){
+				target = null;
+				reachingTarget = false;
+			}
+		}
+
+		if( !isWalking){
+			if(cooldown <= 0 ){
+				rnd = Random.Range(-0f,1f);
+				if( rnd > .5f){
+					Crawl();
+				}else{
+					cooldown = Random.Range(1f, 4f);
+				}
+			}else if(cooldown > 0){
+				cooldown -= Time.deltaTime;
+			}
+		}
+
+		if(agent.velocity.magnitude == 0){
+			isWalking = false;
+		}
+
+	}
+
+	void Crawl(){
+		float crawlRange = 1;
+		isWalking = true;
+		agent.speed = unitSpeed * .2f;
+		randomPos = transform.position + new Vector3(Random.Range(-crawlRange,crawlRange), 0, Random.Range(-crawlRange,crawlRange));
+		NavMesh.CalculatePath(transform.position, randomPos, NavMesh.AllAreas, path);
+		agent.path = path;
+	}
+
+	void Incoming(){
+		isWalking = true;
+		reachingTarget = true;
+		agent.speed = unitSpeed;
+		agent.SetDestination(target.position);
+	}
+
+	private void OnTriggerEnter(Collider detection) {
+		Debug.Log("dupa");
+		string tg = "Player";
+		FindTarget(detection.transform, transform.position, tg);
+	}
+	private void OnTriggerStay(Collider detection) {
+		if(target == null){
+			string tg = "Player";
+			FindTarget(detection.transform, transform.position, tg);
+		}
+	}
+}
---
+++ timeKiller/Assets/Resources/scripts/enemies/enemyMovement.cs
@@ -0,0 +1,49 @@
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.AI;
+
+
+// public class enemyMovement : enemyBehaviours {
+
+// 	public NavMeshAgent agent;
+// 	public bool isMoving;
+// 	private float crawlCooldown;
+// 	// Use this for initialization
+// 	void Start () {
+// 		agent = GetComponent<NavMeshAgent>();
+// 		path = new NavMeshPath();
+// 		destination = transform.position;
+// 		agent.stoppingDistance = .1f;
+// 	}
+	
+// 	// Update is called once per frame
+// 	void Update () {
+
+// 		if(agent.velocity.magnitude < .1f){
+// 			isMoving = false;
+// 		}
+
+// 		if( !isMoving){
+// 			if(crawlCooldown <= 0 ){
+// 				if( Random.Range(-0f,1f) > .1f){
+// 					agent.SetDestination(Crawl(transform.position));
+// 					isMoving = true;	
+// 				}else{
+// 					crawlCooldown = CooldownCounter(3f);
+// 				}
+// 			}else if(crawlCooldown > 0){
+// 				crawlCooldown -= Time.deltaTime;
+// 			}
+// 		}
+		
+// 	}
+
+// 	private IEnumerator WaitAfterCrawl(float t){
+// 		yield return new WaitForSeconds(t);
+// 		if(!isMoving)
+// 			agent.SetDestination(Crawl(transform.position));
+// 		isMoving = true;
+// 	}
+	
+// }
