ef2dee51eed59805806b2e8887f8545136df443d
Borysewicz <33198052+mriperm@gmail.com>
2018-07-15 12:46:01 +0200

    Some small changes to shooting system

---
+++ timeKiller/Assets/Resources/scripts/shootingScripts/Health.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+	public float hitPoints;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void getHit(float damage){
+		this.hitPoints -= damage;
+		if (hitPoints <=0){
+			Destroy(this.gameObject);
+		}
+	}
+}
