2d211c8e652c57002614b5f3db8b438ff84906f2
Borysewicz <33198052+mriperm@gmail.com>
2018-07-18 21:08:04 +0200

    Added ammunition system and basic enemy health script.

---
+++ timeKiller/Assets/Resources/scripts/shootingScripts/AmmunitionSystem.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmunitionSystem : MonoBehaviour {
+
+	
+	public int magazineCapacity;
+	public int currentlyLoaded;
+		
+	public int currentlyCarried;
+	public float reloadTime;
+
+	
+	public bool isMagazineEmpty(){
+		return currentlyLoaded == 0;
+	}
+
+	public bool isMagazineFull(){
+		return currentlyLoaded == magazineCapacity;
+	}
+
+	public bool isCarryingAmmo(){
+		return currentlyCarried > 0;
+	}
+
+
+	public void reload(){
+		if(isCarryingAmmo()){
+			Invoke("fillMagazine",reloadTime);
+			GetComponent<WeaponScript>().setInactive();	
+		}
+	}
+
+	private void fillMagazine(){
+		
+		if (magazineCapacity < (currentlyLoaded+currentlyCarried) ){
+			currentlyCarried -= magazineCapacity;
+			currentlyCarried += currentlyLoaded;
+			currentlyLoaded = magazineCapacity;
+		}else{
+			currentlyLoaded += currentlyCarried;
+			currentlyCarried = 0;
+			}
+		GetComponent<WeaponScript>().setActive();	
+
+	}
+	
+}
